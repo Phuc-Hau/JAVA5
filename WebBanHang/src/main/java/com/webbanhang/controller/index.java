ad230d49f01912b3d7e92ced2d69e5f55218f392
PhucHau <93696783+Phuc-Hau@users.noreply.github.com>
2022-06-08 08:28:45 +0700

    Update index.java

--- WebBanHang/src/main/java/com/webbanhang/controller/index.java
+++ WebBanHang/src/main/java/com/webbanhang/controller/index.java
@@ -9,12 +9,4 @@ import com.webbanhang.model.User;
 @Controller
 public class index {
 
-	@RequestMapping("/index")
-<<<<<<< HEAD
-	public String index(@ModelAttribute("user") User user) {
-=======
-	public String index() {
->>>>>>> 5290559ca5571a76e224a232d420e6351fea4602
-		return "index";
-	}
 }
