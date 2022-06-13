0651aa9ecc91e18ed55d0e4a358fd682b8d4101e
Phuc-Hau <93696783+Phuc-Hau@users.noreply.github.com>
2022-06-13 11:20:34 +0700

    kk

---
+++ WebBanHang/src/main/java/com/webbanhang/controller/CartController.java
@@ -0,0 +1,13 @@
+package com.webbanhang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CartController {
+
+	@RequestMapping("/cart")
+	public String cart() {
+		return "cart";
+	}
+}
